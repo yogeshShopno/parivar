70f636890546b1fde2f566994cd4fe52b4450f00
yogeshShopno <yogesh.shopno@gmail.com>
2026-05-27 11:45:40 +0530

    fix gallery

--- back/src/routes/member/index.js
+++ back/src/routes/member/index.js
@@ -5,13 +5,13 @@ const router = express.Router();
 router.use(require('../authRoutes'));
 router.use(require('../businessRoutes'));
 router.use(require('../directoryRoutes'));
-router.use(require('../postsRoutes'));
+router.use(require('../PostsRoutes'));
 
 router.use('/auth', require('../authRoutes'));
 router.use('/business', require('../businessRoutes'));
 router.use('/config', require('../configRoutes'));
 router.use('/directory', require('../directoryRoutes'));
-router.use('/posts', require('../postsRoutes'));
+router.use('/posts', require('../PostsRoutes'));
 router.use('/news', require('../newsRoutes'));
 router.use('/users', require('./userRoutes'));
 
