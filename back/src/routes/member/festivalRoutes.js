364200ef24546476a27567db6f6ec101d41b2502
Radhi <digi.radhi@gmail.com>
2026-05-28 12:11:55 +0530

    feat: add POST route for saving festivals with form parsing

--- back/src/routes/member/festivalRoutes.js
+++ back/src/routes/member/festivalRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { getFestivals } = require('../../controllers/feedController');
+const adminContent = require('../../controllers/adminContentController');
 const { protect } = require('../../middleware/auth');
+const { parseForm } = require('../../middleware/upload');
 
 const router = express.Router();
 
 router.get('/', protect, getFestivals);
+router.post('/', parseForm, adminContent.saveFestival);
 
 module.exports = router;
