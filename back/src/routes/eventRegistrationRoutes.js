a05d6dc4a7892da44dda3f2e260536eba68523fc
yogeshShopno <yogesh.shopno@gmail.com>
2026-06-10 17:22:48 +0530

    event registration

--- back/src/routes/eventRegistrationRoutes.js
+++ back/src/routes/eventRegistrationRoutes.js
@@ -9,12 +9,22 @@ const {
 } = require('../controllers/eventRegistrationController');
 const { protect } = require('../middleware/auth');
 
+// allow optional authentication: attach req.user if token present
+const optionalProtect = async (req, res, next) => {
+  const { getTokenFromRequest } = require('../middleware/auth');
+  const token = getTokenFromRequest(req);
+  if (token) {
+    return protect(req, res, next);
+  }
+  return next();
+};
+
 router.get('/', protect, getRegistrationsList);
 router.get('/download', protect, downloadRegistrations);  
-router.get('/:id', getRegistrationById);
-router.post('/', addRegistration);
-router.put('/:id', updateRegistration);
-router.patch('/:id/cancel', cancelRegistration);
+router.get('/:id', protect, getRegistrationById);
+router.post('/', optionalProtect, addRegistration);
+router.put('/:id', protect, updateRegistration);
+router.patch('/:id/cancel', protect, cancelRegistration);
 
 
 module.exports = router;
